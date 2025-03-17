b0babbb8b7d65b1f78239a0b7de8bda670f68658
Yesica Navarro <yesi_navarro10@hotmail.com>
2025-03-17 17:19:51 -0300

    Crear Modelos

--- src/blog/models.py
+++ src/blog/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+# Creación de los 3 modelos
+
+class Cliente(models.Model):
+    nombre = models.CharField(max_length=80)
+    apellido = models.CharField(max_length=80)
+    usuario = models.CharField(max_length=80, unique=True)
+    email = models.EmailField(unique=True)
+    nacimiento = models.DateField(null=True, blank=True)
+    direccion = models.TextField(unique=True)
+    
+    def __str__(self) -> str:
+        return f"{self.nombre} {self.apellido} - usuario: {self.usuario}"
+
+class Producto(models.Model):
+    nombre = models.CharField(max_length=50)
+    descripcion = models.TextField(max_length=500)
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.DecimalField(max_digits=20, decimal_places=0)
+    
+    def __str__(self) -> str:
+        return self.nombre
+
+class Pedido(models.Model):
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+    productos = models.ManyToManyField(Producto)
+    fecha_pedido = models.DateTimeField(auto_now_add=True)
+    total = models.DecimalField(max_digits=10, decimal_places=2)
+    id = models.AutoField(primary_key=True)
+    
+    def __str__(self) -> str:
+        return f"Pedido {self.id} - {self.cliente.usuario}"
