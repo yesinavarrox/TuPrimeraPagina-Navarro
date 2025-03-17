e8c5909a8188288406da6222b4f43f87b4a162fa
Yesica Navarro <yesi_navarro10@hotmail.com>
2025-03-17 19:35:31 -0300

    Ultimos detalles

--- src/blog/admin.py
+++ src/blog/admin.py
@@ -3,7 +3,11 @@ from django.contrib import admin
 
 from . import models
 
-admin.site.register(models.Cliente)
+@admin.register(models.Cliente)
+class ClientesAdmin(admin.ModelAdmin):
+    list_display = ("nombre", "apellido", "usuario")
+    search_fields = ("nombre", "apellido", "usuario")
+    ordering = ["apellido"]
 
 @admin.register(models.Producto)
 class ProductosAdmin(admin.ModelAdmin):
@@ -14,6 +18,6 @@ class ProductosAdmin(admin.ModelAdmin):
 @admin.register(models.Pedido)
 class PedidosAdmin(admin.ModelAdmin):
     list_display = ("id", "fecha_pedido", "cliente", "total")
-    search_fields = ("id", "fecha_pedido", "cliente")
+    search_fields = ("fecha_pedido", "cliente__nombre")
     ordering = ["cliente"]
     date_hierarchy = ("fecha_pedido")
